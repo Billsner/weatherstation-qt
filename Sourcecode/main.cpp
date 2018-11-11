88d8d64bf25b7f4d54041694d4e383988092dc6c
Billsner <matzerudolf@gmx.de>
2018-11-11 13:46:23 +0100

    Fix bug during shutdown --> add sleep for new thread

--- Sourcecode/main.cpp
+++ Sourcecode/main.cpp
@@ -45,15 +45,16 @@ int main(int argc, char *argv[])
     //start WeatherThread
     WeatherThread mWeatherThread;
     mWeatherThread.initthread();
-    //mWeatherThread.start(QThread::NormalPriority);
+    mWeatherThread.start(QThread::NormalPriority);
 
     app.exec();
     logger <= "app.exec";
 
     logger <= "thread exit";
     mDatapoolThread.quit();
-    //mWeatherThread.quit();
-    mDatapoolThread.wait(100); // todo check if it is valid to call wait after quit
+    mDatapoolThread.wait(10); // todo check if it is valid to call wait after quit
+    mWeatherThread.quit();
+    mWeatherThread.wait(10);
     GetQMLObject::DestroyGetQMLObject();
     cLoggingServer.finish();
     return 1;
